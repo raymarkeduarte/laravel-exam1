8a81c3c0193e739ad3c02fe9e361068dedcfdb5b
Raymark <raymarkedelacruz@gmail.com>
2023-01-26 16:29:20 +0800

    final-UI

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Exam 1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <style>
+        a{
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
     <br><br><br><br><br>
@@ -16,16 +21,18 @@
                 *
             </div>
             <div class="card-body">
-                <h5 class="card-title">Member Login</h5>
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="username" placeholder=" ">
-                    <label for="username">Username</label>
-                </div>
-                <div class="form-floating mb-3">
-                    <input type="password" class="form-control" id="password" placeholder="Password">
-                    <label for="password">Password</label>
-                </div>
-                <button type="button" class="btn btn-primary">Login</button>
+                <form action="" method="post">
+                    <h5 class="card-title">Member Login</h5>
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="username" placeholder="username" required>
+                        <label for="username">Username</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="password" class="form-control" id="password" placeholder="Password" required>
+                        <label for="password">Password</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100">Login</button>
+                </form>
             </div>
             <div class="card-footer text-muted">
                 <a href="#" class="">Forget Password?</a>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,7 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Exam 1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        a{
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
     <br><br><br><br><br>
@@ -16,48 +22,52 @@
                 <h1 class="card-title">Register</h1>
             </div>
             <div class="card-body">
-                <p>Create your account. It's free and only takes a minute.</p>
+                <form action="" method="post">
 
-                <div class="row g-2">
-                    <div class="col-md mb-3">
-                        <div class="form-floating">
-                            <input type="text" class="form-control" id="first_name" placeholder=" " value="">
-                            <label for="first_name">First Name</label>
+                    <p>Create your account. It's free and only takes a minute.</p>
+
+                    <div class="row g-2">
+                        <div class="col-md mb-3">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="first_name" placeholder=" " value="" required>
+                                <label for="first_name">First Name</label>
+                            </div>
+                        </div>
+                        <div class="col-md mb-3">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="first_name" placeholder=" " value="" required>
+                                <label for="last_name">Last Name</label>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md mb-3">
-                        <div class="form-floating">
-                            <input type="text" class="form-control" id="first_name" placeholder=" " value="">
-                            <label for="last_name">Last Name</label>
-                        </div>
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="username" placeholder=" " required>
+                        <label for="username">Username</label>
                     </div>
-                </div>
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="username" placeholder=" ">
-                    <label for="username">Username</label>
-                </div>
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="email" placeholder=" ">
-                    <label for="email">Email</label>
-                </div>
-                <div class="form-floating mb-3">
-                    <input type="password" class="form-control" id="cpassword" placeholder="Password">
-                    <label for="cpassword">Password</label>
-                </div>
-                <div class="form-floating mb-3">
-                    <input type="password" class="form-control" id="cpassword" placeholder="Password">
-                    <label for="cpassword">Confirm Password</label>
-                </div>
-                <div class="form-check text-start mb-3">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                    <label class="form-check-label" for="flexCheckDefault">
-                        I accept the Terms of Use & Privacy Policy
-                    </label>
-                </div>
-                <button type="button" class="btn btn-primary">Register Now</button>
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="email" placeholder=" " required>
+                        <label for="email">Email</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="password" class="form-control" id="cpassword" placeholder="Password" required>
+                        <label for="cpassword">Password</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="password" class="form-control" id="cpassword" placeholder="Password" required>
+                        <label for="cpassword">Confirm Password</label>
+                    </div>
+                    <div class="form-check text-start mb-3">
+                        <input class="form-check-input" type="checkbox" value="" id="terms" required>
+                        <label class="form-check-label" for="terms">
+                            I accept the Terms of Use & Privacy Policy
+                        </label>
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100">Register Now</button>
+
+                </form>
             </div>
             <div class="card-footer text-muted">
-                <a href="../" class="">Login</a>
+                <a href="../" class=""><i class="fa-solid fa-backward-step"></i> Go back to Login</a>
             </div>
         </div>
     </div>
